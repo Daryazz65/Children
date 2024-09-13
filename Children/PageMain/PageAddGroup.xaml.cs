f7c77c90b82102a3a5bff0996105cee6f343cf54
Дарья Зинина <zininadashuta167@gmail.com>
2024-09-13 09:44:09 +0300

    jcgj

--- Children/PageMain/PageAddGroup.xaml.cs
+++ Children/PageMain/PageAddGroup.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Children.Model;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,42 @@ namespace Children.PageMain
         public PageAddGroup()
         {
             InitializeComponent();
+
+            CmbVG.SelectedValuePath = "ID";
+            CmbVG.DisplayMemberPath = "Name";
+            CmbVG.ItemsSource = App.context.ViewGroup.ToList();
+        }
+
+        private void AddGroup_Click(object sender, RoutedEventArgs e)
+        {
+            string mes = "";
+            if (string.IsNullOrWhiteSpace(TxtGroup.Text))
+                mes += "Введите группу\n";
+
+            if (string.IsNullOrWhiteSpace(CmbVG.Text))
+                mes += "Выберите вид группы\n";
+            if (mes != "")
+            {
+                MessageBox.Show(mes);
+                mes = "";
+                return;
+            }
+            Group groupDS = new Group()
+            {
+                Name = TxtGroup.Text,
+                ViewGroup = CmbVG.SelectedItem as ViewGroup
+            };
+            App.context.Group.Add(groupDS);
+            App.context.SaveChanges();
+            MessageBox.Show("Группа добавлена");
+
+            TxtGroup.Text = "";
+            CmbVG.Text = "";
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            ClassFrame.FrameBody.GoBack();
         }
     }
 }
--- Children/Windows/MainWindow.xaml.cs
+++ Children/Windows/MainWindow.xaml.cs
@@ -23,6 +23,8 @@ namespace Children
         public MainWindow()
         {
             InitializeComponent();
+            
+
         }
     }
 }
